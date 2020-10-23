0733eaba269a0cc75a56488f149e8332d3aedc87
bhumish18 <72477889+bhumish18@users.noreply.github.com>
2020-10-23 15:28:27 -0400

    Add files via upload

---
+++ inputs.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class inputs {
+	
+	
+	public String text(){
+		Scanner input = new Scanner(System.in);
+        String xtext = input.next();
+        input.close();
+        return xtext;
+        
+	    }
+	
+}
