eb7d05e80978a86d4750b2f715f84d549a91d62c
Surya-Munuganti <61526517+Surya-Munuganti@users.noreply.github.com>
2020-12-04 13:57:51 -0500

    Update editProfile.java

--- packages/Edit/editProfile.java
+++ packages/Edit/editProfile.java
@@ -1,4 +1,4 @@
-package edit;
+ppackage edit;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -13,6 +13,10 @@ import javax.servlet.http.HttpSession;
 import PostAd.DatabaseConnection;
 
 
+
+/*
+ * This servlet is used when users wants to update their information 
+ */
 @WebServlet("/editProfile")
 public class editProfile extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -29,7 +33,7 @@ public class editProfile extends HttpServlet {
 			long updPhone_Number = Long.parseLong(request.getParameter("updPhone_Number"));
 			String updUser_Name = request.getParameter("updUser_Name");
 			String updup = request.getParameter("updup");
-
+//The query updates the ads_users table which stores the information about the users
 			String query1 = "UPDATE virtualmart.users SET First_Name = ?, Last_Name = ?, Email = ?, Phone_Number = ?, User_Name = ?, User_Pass = ? WHERE User_ID = ?";
 			PreparedStatement stmnt1 = conn.prepareStatement(query1);
 			stmnt1.setString(1, updFirst_Name);
@@ -39,11 +43,19 @@ public class editProfile extends HttpServlet {
 			stmnt1.setString(5, updUser_Name);
 			stmnt1.setString(6, updup);
 			stmnt1.setInt(7, UserIDinfo);
-
+//Update the database with newly entered and unchanged details by the user
 			stmnt1.executeUpdate();
-			request.getRequestDispatcher("UserResponse.jsp").forward(request, response);
-
-		}catch(Exception e) {
+//Based on the userId differentiate between the admin and user and redirect accordingly
+			if(UserIDinfo==2) {
+				request.getRequestDispatcher("AdminWelcome.jsp").forward(request, response);
+			}
+			else {
+			request.getRequestDispatcher("AfterEditProfile.jsp").forward(request, response);
+			}
+		}
+		
+		
+		catch(Exception e) {
 			System.out.println(e);
 		}
 
